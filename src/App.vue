1f458031686ce8bfab7b4eb666c1955a3e8e35ab
Fenix <savchenko@evergreens.in.ua>
2017-10-16 02:19:58 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>Word Translator</h1>
+    <h5>Powered by Vue JS</h5>
+    <TranslateForm v-on:formSubmit="translateText"></TranslateForm>
+     <TranslateOutput v-text="translatedText"></TranslateOutput>
+  </div>
+</template>
+
+<script>
+import TranslateForm from './components/TranslateForm'
+import TranslateOutput from './components/TranslateOutput';
+export default {
+  name: 'app',
+  components:{
+    TranslateForm,
+    TranslateOutput
+  },
+  data: function(){
+    return {
+      translatedText:''
+    }
+  },
+  methods: {
+    translateText:function(text, language){
+      this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170329T180255Z.3c18d2dc7b65d525.f23ed9a9efa992bded4ef96334e3c154f61d2dea&lang='+language+'&text='+text+'&option=1')
+      .then((response) => {
+        this.translatedText = response.body.text[0];
+        console.log(response);
+      });
+    }
+  }
+}
+</script>
---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="row" id="translateForm">
+    <div class="col-md-6 col-md-offset-3">
+      <form id="transForm" class="form-inline well" v-on:submit="formSubmit">
+        <input class="form-control" type="text" v-model="textToTranslate" placeholder="Enter a Word...">
+        <input class="form-control" type="text" v-model="language" placeholder="Enter a lang">
+        <!-- <select class="form-control" v-model="language">
+          <option value="ru">Russian</option>
+          <option value="es">Spanish</option>
+          <option value="fr">French</option>
+          <option value="zh">Chinese</option>
+        </select> -->
+        <input class="btn btn-primary" type="submit" value="Translate">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'translateForm',
+  data(){
+    return {
+      textToTranslate:'',
+      language:''
+    }
+  },
+  created() {
+    this.language = 'ru';
+  },
+  watch: {
+    textToTranslate: function () {
+      this.formSubmit()
+    }
+  },
+  methods: {
+    formSubmit(e){
+      this.$emit('formSubmit', this.textToTranslate, this.language);
+      e.preventDefault();
+    },
+    expensiveOperation(){
+      console.log('expensiveOperation');
+    }
+  }
+}
+</script>
